d19b1d77b91de5128ae1bd1e4491dca78a94b439
ishikawa tatsuro <ishitatsu820@gmail.com>
2020-06-26 10:12:25 +0900

    sidebar.phpの作成。

--- mypage.php
+++ mypage.php
@@ -126,21 +126,9 @@ debug('処理終わり <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
     
     
     <!-- サイドバー -->
-    <div class="sidebar">
-      <div class="sidebar-item">
-        <a href="">新規投稿</a> 
-      </div>
-      <div class="sidebar-item">
-        <a href="">プロフィール編集</a>
-      </div>
-      <div class="sidebar-item">
-        <a href="">パスワード変更</a>
-      </div>
-      <div class="sidebar-item">
-        <a href="">退会</a>
-      </div>
-    </div>
-  </div>
+    <?php
+      require('sidebar.php');
+    ?>
     
     
   <!-- フッター -->
